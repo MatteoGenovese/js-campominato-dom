8604eb735244661cffb9cb05f7ec87ff7bce92fd
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-24 14:48:41 +0200

    bombList created

--- js/script.js
+++ js/script.js
@@ -6,46 +6,81 @@ const difficultyContainer = document.getElementById('difficultySelected');
 
 const generatorButton = document.getElementById('generator');
 
-generateGrid(3);
+generateGrid(1);
+
 
 // | creo un bottone che mi permette di aggiornare la griglia
 generatorButton.addEventListener('click', function() {
     generateGrid(getSelectValue());
+
 });
 
 // Ciclo per il numero di difficoltà che voglio generare
 for (let i = 0; i < 3; i++) {
-    const newDifficulty = createNewDifficulty(3 - i);
+    const newDifficulty = createNewDifficulty(i + 1);
     addEventListenerForDifficulty(newDifficulty);
     difficultyContainer.append(newDifficulty);
 }
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------FUNCTIONS*/
+function generateBombs(numberOfSquares, start) {
+
+    const gridContainer = document.getElementById('grid-container');
+
+    let bombList = [];
+    let newRandomNumber;
+    let i = 0;
+
+    // finché il numero trovato non è valido
+    while (bombList.length < 16) {
+        // genera un nuovo numero randomico nell'intervallo min-max
+        newRandomNumber = Math.floor(Math.random() * (numberOfSquares + 1) - start) + start;
+
+        // se non è già presente in blacklist || ovvero che il numero è nuovo e valido
+        if (!bombList.includes(newRandomNumber)) {
+            // usciamo dal ciclo
+            bombList[i] = newRandomNumber;
+            i++;
+        }
+    }
+
+    console.log(bombList);
+    // currentBlackList.push(i); // | e lo aggiungo alla blacklist
+    // return newRandomNumber;
+
+
+
+}
 
 function generateGrid(difficultyValue) { // | ciclo per il numero di quadrati che voglio generare
 
     document.getElementById('grid-container').innerHTML = "";
     const gridContainer = document.getElementById('grid-container');
-    const currentBlackList = []; // | creo una lista vuota, che sarà la lista degli elementi già estratti
-    if (difficultyValue == 3) {
-        numberOfSquares = 100;
-    } else if (difficultyValue == 2) {
-        numberOfSquares = 81;
-    } else if (difficultyValue == 1) {
-        numberOfSquares = 49;
+    const bombList = []; // | creo una lista vuota, che sarà la lista delle bombe ritornate
+    switch (getSelectValue()) {
+        default: numberOfSquares = 100;
+        case '1':
+                numberOfSquares = 100;
+            break;
+        case '2':
+                numberOfSquares = 81;
+            break;
+        case '3':
+                numberOfSquares = 49;
+            break;
     }
-
 
     for (let i = 0; i < numberOfSquares; i++) {
         const newSquare = createNewSquare(difficultyValue); // # creo un nuovo quadrato con le classi relative
-        const newUniqueNum = generateUniqueRandomNumber(currentBlackList, 0, numberOfSquares - 1); // | mi genero un nuovo numero randomico che non sia già stato estratto
-        newSquare.innerHTML = newUniqueNum; // il contenuto del quadrato sarà il numero randomico unico appena generato
-        let className = (newUniqueNum % 2 === 0) ? 'cyaned' : 'redned'; // ! in base al valore di parità del numero randomico unico appena generato assegnerò un toggle con classi diverse
-        addEventListenerWithToggle(newSquare, className, newUniqueNum);
+        newSquare.innerHTML = i;
+        let className = (i % 2 === 0) ? 'cyaned' : 'redned'; // ! in base al valore di parità del numero randomico unico appena generato assegnerò un toggle con classi diverse
+        addEventListenerWithToggle(newSquare, className, i);
         gridContainer.append(newSquare); // § aggiungo il nuovo quadrato al parent
-        currentBlackList.push(newUniqueNum); // | e lo aggiungo alla blacklist
     }
+    generateBombs(numberOfSquares, 0);
 }
+
+
 
 function getSelectValue() {
     var selectedValue = document.getElementById("difficultySelected").value;
@@ -64,7 +99,6 @@ function addEventListenerForDifficulty(htmlElement) {
 
     htmlElement.addEventListener('click', function() {
         htmlElement.classList.toggle("selection");
-        console.log(htmlElement)
     });
 }
 // console.log(`è stata cliccata la cella numero ${cellNumber}`);
@@ -81,24 +115,24 @@ function createNewSquare(difficultyValue) {
     return currentSquare;
 }
 
-// funzione: prende blacklist, prende il valore minimo, il valore massimo inclusi
-function generateUniqueRandomNumber(blackList, min, max) {
-    let newRandomNumber;
-    let isNumberValid = false;
+// // funzione: prende blacklist, prende il valore minimo, il valore massimo inclusi
+// function generateUniqueRandomNumber(blackList, min, max) {
+//     let newRandomNumber;
+//     let isNumberValid = false;
 
-    // finché il numero trovato non è valido
-    while (isNumberValid === false) {
-        // genera un nuovo numero randomico nell'intervallo min-max
-        newRandomNumber = Math.floor(Math.random() * (max + 1) - min) + min;
+//     // finché il numero trovato non è valido
+//     while (isNumberValid === false) {
+//         // genera un nuovo numero randomico nell'intervallo min-max
+//         newRandomNumber = Math.floor(Math.random() * (max + 1) - min) + min;
 
-        // se non è già presente in blacklist || ovvero che il numero è nuovo e valido
-        if (!blackList.includes(newRandomNumber)) {
-            // usciamo dal ciclo
-            isNumberValid = true;
-        }
-    }
-    return newRandomNumber;
-}
+//         // se non è già presente in blacklist || ovvero che il numero è nuovo e valido
+//         if (!blackList.includes(newRandomNumber)) {
+//             // usciamo dal ciclo
+//             isNumberValid = true;
+//         }
+//     }
+//     return newRandomNumber;
+// }
 
 // Operatore ternario:
 // § condizione ? valoreSeLaCondizioneÈVera : valoreSeLaCondizioneÈFalsa;
